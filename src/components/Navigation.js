274664bb24639deda4d0c0e4091edc5c9f775ea7
Dave Vazquez <dmvaz.li@gmail.com>
2020-07-20 13:30:29 -0400

    replaces styled-component wrapper with scss file

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,15 @@
+import "./styles/navigation.scss";
+
 import React from "react";
-import styled from "styled-components";
 import { ReactComponent as HomeSVG } from "../assets/home.svg";
 import { ReactComponent as LogoSVG } from "../assets/nearpod-logo.svg";
 
 const Navigation = () => {
   return (
-    <Container>
+    <div className="mock-navigation">
       <HomeSVG /> <LogoSVG />
-    </Container>
+    </div>
   );
 };
-
-const Container = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-
-  padding: 1.6rem;
-
-  border-bottom: 1px solid #eaeef1;
-  box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.28);
-
-  width: 100%;
-  height: 8.9rem;
-
-  .nearpod-logo {
-    margin-left: 1rem;
-  }
-`;
 
 export default Navigation;
