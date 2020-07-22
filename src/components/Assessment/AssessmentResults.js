b44ce19dc4f7bf635c09f8e1514928b5e11d62e1
Dave Vazquez <dmvaz.li@gmail.com>
2020-07-22 12:44:53 -0400

    gh-pages, pls work...

--- src/components/Assessment/AssessmentResults.js
+++ src/components/Assessment/AssessmentResults.js
@@ -3,7 +3,6 @@ import "./styles/assessment-results.scss";
 import _ from "lodash";
 import React from "react";
 import { Link, Route } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 import Analysis from "./Analysis/Analysis";
 import Summary from "./Summary/Summary";
 
@@ -11,47 +10,41 @@ const AssessmentResults = (props) => {
   const { questions, students, scores } = props.assessment;
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <section className="assessment-results">
-        <header>
-          <h2>Results</h2>
-        </header>
-        <div>
-          <nav>
-            <Link to="/">Summary</Link>
-            {_.map(questions, (question) => {
-              return (
-                <Link key={question.id} to={`/${question.id}`}>
-                  Question {question.id + 1}
-                </Link>
-              );
-            })}
-          </nav>
-          <Route
-            exact
-            path="/"
-            render={(props) => (
-              <Summary {...props} students={students} scores={scores} />
-            )}
-          />
+    <section className="assessment-results">
+      <header>
+        <h2>Results</h2>
+      </header>
+      <div>
+        <nav>
+          <Link to="/">Summary</Link>
           {_.map(questions, (question) => {
             return (
-              <Route
-                key={question.id}
-                path={`/${question.id}`}
-                render={(props) => (
-                  <Analysis
-                    {...props}
-                    question={question}
-                    students={students}
-                  />
-                )}
-              />
+              <Link key={question.id} to={`/${question.id}`}>
+                Question {question.id + 1}
+              </Link>
             );
           })}
-        </div>
-      </section>
-    </Router>
+        </nav>
+        <Route
+          exact
+          path="/"
+          render={(props) => (
+            <Summary {...props} students={students} scores={scores} />
+          )}
+        />
+        {_.map(questions, (question) => {
+          return (
+            <Route
+              key={question.id}
+              path={`/${question.id}`}
+              render={(props) => (
+                <Analysis {...props} question={question} students={students} />
+              )}
+            />
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
